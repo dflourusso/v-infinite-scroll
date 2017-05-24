e41226adcf2f71eed0da59ef48ebd98f4b87969d
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-05-24 12:10:01 -0300

    better demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,26 @@
 <template lang="pug">
 div(style="text-align: center;")
-  h2 Move the scroll up and down do load next or previous pages
-  infinite-scroll.infinite-scroll(:loading='loading', @top='prevPage', @bottom='nextPage', :offset='0')
-    div(v-for='item in items')
-      p.id {{item.id}}
-      p.description {{item.description}}
-    h1.loading-icon(v-if='loading') &#x21bb;
+  div(style="text-align: left;")
+    input(type="radio" id="one", :value="true" v-model="showNormal")
+    label(for="one") &nbsp;Normal Scroll
+    br
+    input(type="radio" id="two", :value="false" v-model="showNormal")
+    label(for="two") &nbsp;Bidirectional Scroll
+  div(v-if='showNormal')
+    h2 Move the scroll down do load next pages
+    infinite-scroll.infinite-scroll(:loading='loading2', @bottom='nextPage2', :offset='300')
+      div(v-for='item in items2')
+        p.id {{item.id}}
+        p.description {{item.description}}
+      h1.loading-icon(v-if='loading2') &#x21bb;
+  div(v-else)
+    h2 Move the scroll up and down do load next or previous pages
+    infinite-scroll.infinite-scroll(:loading='loading1', @top='prevPage1', @bottom='nextPage1', :offset='10')
+      div(v-for='item in items1')
+        p.id {{item.id}}
+        p.description {{item.description}}
+      h1.loading-icon(v-if='loading1') &#x21bb;
+
 </template>
 
 <script>
@@ -16,40 +31,74 @@ export default {
   data () {
     return {
       loremConstant: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      page: 1,
-      items: [],
-      loading: false
+      showNormal: true,
+      page1: 1,
+      items1: [],
+      loading1: false,
+      page2: 1,
+      items2: [],
+      loading2: false
     }
   },
   methods: {
-    prevPage () {
-      if (this.page == 1) return
-      --this.page
-      this.api(this.page)
+    prevPage1 () {
+      if (this.page1 == 1) return
+      --this.page1
+      this.api1(this.page1)
     },
-    nextPage () {
-      ++this.page
-      this.api(this.page)
+    nextPage1 () {
+      ++this.page1
+      this.api1(this.page1)
     },
-    api (page = 1, limit = 20) {
-      this.loading = true
+    api1 (page = 1, limit = 20) {
+      this.loading1 = true
       --page
       if (page <= 0) page = 0
-      this.items = []
+      this.items1 = []
       setTimeout(() => {
         let items = []
         let offset = page * limit
         for (let i = offset; i < offset+limit; i++) {
           items.push({id: i, description: this.loremConstant})
         }
-        this.items = items
-        this.loading = false
+        this.items1 = items
+        this.loading1 = false
       }, 1000)
+
+    },
+    nextPage2 () {
+      ++this.page2
+      this.api2(this.page2)
+    },
+    api2 (page = 1, limit = 20) {
+      this.loading2 = true
+      --page
+      if (page <= 0) page = 0
+      setTimeout(() => {
+        let items = []
+        let offset = page * limit
+        for (let i = offset; i < offset+limit; i++) {
+          items.push({id: i, description: this.loremConstant})
+        }
+        this.items2 = this.items2.concat(items)
+        this.loading2 = false
+      }, 500)
 
     }
   },
   created () {
-    this.api()
+    this.api1()
+    this.api2()
+  },
+  watch: {
+    showNormal () {
+      if (this.showNormal) {
+        this.items2 = []
+        this.api2()
+      } else {
+        this.api1(1)
+      }
+    }
   }
 }
 </script>
